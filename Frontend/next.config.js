936e1e1e2dd31ac720ec0813d9001717e59fe6d2
Sidd2712 <siddharth.2727goyal@gmail.com>
2024-10-21 23:16:19 +0530

    first commit

---
+++ Frontend/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: false,
+};
+
+// const withFonts = require('next-fonts');
+// module.exports = withFonts({
+//   enableSvg: true,
+//   webpack(config, options) {
+//     return config;
+//   }
+// });
+
+module.exports = nextConfig;
